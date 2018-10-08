93f8af97e8d13d13a2f09101f6dc9d792f84ce2a
Austin Rude <rudedogg@gmail.com>
2018-10-08 14:48:40 -0600

    Initial commit

---
+++ WKWebViewBug/ViewController.swift
@@ -0,0 +1,51 @@
+import Cocoa
+import WebKit
+
+class ViewController: NSViewController {
+  @IBOutlet weak var webView: WKWebView!
+
+  @IBAction func bundleLoadClicked(_ sender: NSButton) {
+    guard let indexFile = Bundle.main.url(forResource: "bundle", withExtension: "html", subdirectory: nil) else {
+      return
+    }
+    print("Loading demo.html from bundle, url: \(indexFile)")
+    webView.loadFileURL(indexFile, allowingReadAccessTo: indexFile)
+  }
+
+  @IBAction func localLoadClicked(_ sender: NSButton) {
+    let openPanel = NSOpenPanel()
+    openPanel.allowedFileTypes = ["html", "htm"]
+    openPanel.canChooseFiles = true
+    openPanel.canChooseDirectories = false
+    openPanel.allowsOtherFileTypes = false
+    openPanel.allowsMultipleSelection = false
+
+    let response = openPanel.runModal()
+    guard response == .OK else {
+      // user pressed cancel
+      return
+    }
+
+    guard let fileURL = openPanel.url else {
+      print("Failed to get file URL from OpenPanel url property.")
+      return
+    }
+
+    print("Loading local file, url: \(fileURL)")
+    webView.loadFileURL(fileURL, allowingReadAccessTo: fileURL.deletingLastPathComponent())
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    webView.navigationDelegate = self
+  }
+}
+
+extension ViewController: WKNavigationDelegate {
+  func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+    // You'll hit this if you load a bundle file, and try to load a local file afterword.
+    print("Navigation failed with error: \(error)")
+  }
+
+}
